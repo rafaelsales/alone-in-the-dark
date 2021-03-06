89c569b593f8aea61da7f70cf81bf21af20179f0
Rafael Sales <rafaelcds@gmail.com>
2021-03-06 20:24:58 -0300

    Make service install executable

--- install-service.sh
+++ install-service.sh
@@ -1,1 +1,1 @@
-sudo cp /home/pi/alone-in-the-dark/alone-in-the-dark /etc/systemd/system/alone-in-the-dark
+sudo cp /home/pi/alone-in-the-dark/alone-in-the-dark /etc/systemd/system/alone-in-the-dark.service
